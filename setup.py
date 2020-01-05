eb8198d3eaa52bd105d27c761f2dad422b7901f0
J. Michael Burgess <jmichaelburgess@gmail.com>
2020-01-05 08:43:23 +0100

    working

--- setup.py
+++ setup.py
@@ -3,19 +3,22 @@ from Cython.Build import cythonize
 import numpy
 
 basic_ext = Extension(
-    'electradpy/sic_powerlaw',
-    sources = ['csource/src/radiator.cxx','csource/src/synchrotron_impulsive_continuous.cxx'],
+    'electradpy/sic_glue',
+    sources=[
+        'csource/sic_glue.pyx', 'csource/src/radiator.cxx',
+        'csource/src/synchrotron_impulsive_continuous.cxx'
+    ],
     language='c++',
     include_dirs=[numpy.get_include()],
-    extra_compile_args=['-I./csource/incl', "-I/usr/local/include", "-Ofast", "-std=c++11"],
-    extra_link_args=["-L/usr/local/lib", '-lgsl', '-lgslcblas']
-)
+    extra_compile_args=[
+        '-I./csource/incl', "-I/usr/local/include", "-Ofast", "-std=c++11",
+        '-stdlib=libc++'
+    ],
+    extra_link_args=["-L/usr/local/lib", '-lgsl', '-lgslcblas'])
 
 ext_modules = [basic_ext]
 
-setup(ext_modules=cythonize(ext_modules),
-      name='elextradpy'
-
-)
-
-
+setup(
+    ext_modules=cythonize(ext_modules),
+    name='electradpy',
+    packages=['electradpy'])
